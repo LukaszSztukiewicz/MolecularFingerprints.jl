ea7eaf84772fd4bdc734472a9602169de52ffe33
Lukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2025-12-08 19:52:17 +0100

    refactor(types): added abstract types

---
+++ src/interface.jl
@@ -0,0 +1,4 @@
+# Abstract Interfaces for Fingerprint Calculators
+abstract type AbstractCalculator end
+abstract type AbstractFingerprint <: AbstractCalculator end
+abstract type AbstractDescriptor <: AbstractCalculator end
