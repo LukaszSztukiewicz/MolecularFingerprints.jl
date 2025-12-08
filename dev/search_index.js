0002f55ac57b24e5bbc36a5eb3156c0b286ed224
Documenter.jl <documenter@juliadocs.github.io>
2025-12-08 17:50:24 +0000

    build based on 87449d9

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#MolecularFingerprints","page":"Home","title":"MolecularFingerprints","text":"Documentation for MolecularFingerprints.\n\n","category":"section"}]
+[{"location":"testing/","page":"-","title":"-","text":"Since our native Julia implementation uses Julia's hash() function, the specific bits set will differ from RDKit's C++ implementation (which uses specific random seeds). Therefore, strict bitwise equality (==) is impossible to test.\n\nInstead, we validate by Ranking Correlation: We verify that MolecularFingerprints.jl identifies the same molecules as \"similar\" that RDKit does. If the chemical logic is correct, the Tanimoto similarity scores for a set of molecules should be highly correlated between the two libraries.","category":"section"},{"location":"#MolecularFingerprints","page":"Home","title":"MolecularFingerprints","text":"Documentation for MolecularFingerprints.\n\n","category":"section"}]
 }
