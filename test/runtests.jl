a073b190edb8a806bbadeef1555f5fdb0f96568a
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2025-12-15 12:34:50 +0100

    added ecfp dummy test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 @testset "MolecularFingerprints.jl" begin
     @testset "Unit Tests" begin
         include("unit/utils/tanimoto_tests.jl")
+        include("unit/algorithms/ecfp_tests.jl")
     end
 end
---
+++ test/unit/algorithms/ecfp_tests.jl
@@ -0,0 +1,11 @@
+using MolecularFingerprints
+using Test
+
+@testset "ECFP Fingerprint Tests" begin
+
+    # dummy test to ensure the test file is being executed
+    # Replace with actual ECFP fingerprint tests when implemented
+
+    @test true
+
+end
