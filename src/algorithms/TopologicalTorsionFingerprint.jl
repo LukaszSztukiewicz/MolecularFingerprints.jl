17eba5bf3f7bc617a2e14cc2f3bfffad077581b6
Jule <juliane.pilgrim@tu-berlin.de>
2025-12-15 19:27:31 +0100

    Removing unnecessary parameter maxdev

--- src/algorithms/TopologicalTorsionFingerprint.jl
+++ src/algorithms/TopologicalTorsionFingerprint.jl
@@ -4,13 +4,11 @@ include("../interface.jl")
 
 struct TopologicalTorsion{N} <: AbstractFingerprint
     radius::Int
-    maxDev::String
 end
 
 function fingerprint(mol::SMILESMolGraph, calc::TopologicalTorsion{N}) where N
     # Placeholder implementation for Topological Torsion fingerprint calculation
     # computes the Topological Torsion fingerprint
-    # based on the molecular structure, the specified symmetry radius and the 
-    # maximal deviation used for normalization.
+    # based on the molecular structure and the specified radius.
     return BitVector(rand(Bool, 1024))  # Example: return a random 1024-bit fingerprint
 end
