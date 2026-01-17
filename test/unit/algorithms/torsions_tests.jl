93239346db48a03cc9ee3033da7bed489039c944
Jule <juliane.pilgrim@tu-berlin.de>
2026-01-17 22:46:00 +0100

    add implementation of topological torsion fingerprint

--- test/unit/algorithms/torsions_tests.jl
+++ test/unit/algorithms/torsions_tests.jl
@@ -3,13 +3,21 @@ using MolecularGraph
 using MolecularFingerprints
 
 @testset "Topological Torsion Fingerprint Tests" begin
+    @testset "testPathFinder" begin
+            data = "c1ccccc1"
+            mol = smilestomol(data)
+            pathList = [[1,2,3,4], [2,3,4,5], [3,4,5,6], [4,5,6,1], [5,6,1,2], [6,1,2,3]]
+            
+            paths = MolecularFingerprints.getPathsOfLengthN(mol, 4)
+            @info paths
+            @test length(pathList) == length(paths)
+            #@test pathList == paths
+        end
 
-    # dummy test to ensure the test file is being executed
-    # Replace with actual Topological Torsion fingerprint tests when implemented
-
-    mol = MolecularGraph.smilestomol("CC(=O)O")
-    torsion_calc = TopologicalTorsion{2}(2)  # Example type TopologicalTorsion{2} with radius 2
-    fp = fingerprint(mol, torsion_calc)
-    @test length(fp) == 1024
+    
+    
+        # torsion_calc = TopologicalTorsion(4)  # Example type TopologicalTorsion with path length 4
+        # mol = MolecularGraph.smilestomol(data)
+        # fp = fingerprint(mol, torsion_calc)
 
 end
