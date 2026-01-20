fa87eba6046152160de54c6d4765aa792348965c
Lukasz Sztukiewicz <sztukiewicz.lukasz@gmail.com>
2026-01-20 12:12:57 +0100

    refactor(dependencies): removed test dependency environment, now it is part of main environment as extra dependency

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,31 +3,53 @@ using MolecularGraph
 using MolecularFingerprints
 using Random
 using SHA
-using Graphs: vertices, induced_subgraph, neighborhood
-
-using Test
-using MolecularGraph
-using MolecularFingerprints
-
-using PythonCall: Py, pyimport, pyconvert, pybuiltins
-using Graphs: nv, all_simple_paths, degree
-
-using Test
-using PythonCall
-using MolecularGraph
-using MolecularFingerprints
 using SparseArrays
+using Graphs: nv, all_simple_paths, degree
+using Graphs: vertices, induced_subgraph, neighborhood
+using PythonCall: Py, pyimport, pyconvert, pybuiltins
 
-using Test
-using MolecularGraph
-using MolecularFingerprints
 
 @testset "MolecularFingerprints.jl" begin
+
     @testset "Unit Tests" begin
-        include("unit/utils/tanimoto_tests.jl")
-        include("unit/algorithms/ecfp_tests.jl")
-        include("unit/algorithms/mhfp_tests.jl")
-        include("unit/algorithms/maccs_tests.jl")
-        include("unit/algorithms/torsions_tests.jl")
+
+        @testset "Interface Tests" begin
+            @info "Running Interface Tests..."
+            include("unit/interface/mock_interface_tests.jl")
+        end
+
+        @testset "Utility Function Tests" begin
+            @info "Running Utility Function Tests..."
+            include("unit/utils/tanimoto_tests.jl")
+        end
+
+        @testset "Fingerprint Algorithm Tests" begin
+            @info "Running Fingerprint Algorithm Tests..."
+
+            @testset "ECFP Tests" begin
+                @info "Running ECFP Tests..."
+                include("unit/algorithms/ecfp_tests.jl")
+            end
+
+            @testset "MHFP Tests" begin
+                @info "Running MHFP Tests..."
+                include("unit/algorithms/mhfp_tests.jl")
+            end
+
+            @testset "MACCS Tests" begin
+                @info "Running MACCS Tests..."
+                include("unit/algorithms/maccs_tests.jl")
+            end
+
+            @testset "Topological Torsion Tests" begin
+                @info "Running Topological Torsion Tests..."
+                include("unit/algorithms/torsions_tests.jl")
+            end
+        end
+    end
+
+    @testset "Reference Tests (Validation)" begin
+        @info "Running Validation Reference Tests..."
+        include("validation/rdkit_comparison_tests.jl")
     end
 end
